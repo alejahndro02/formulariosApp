04e7f3151a65665d51e6f71ec952a3fd5ba8c36c
alejahndro02 <alejahndro02@gmail.com>
2022-02-26 20:10:28 -0600

    se creo una interface y se decararon las ariables templeteMenu y reactiveMenu que son de tipo MenuItem

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+ 
+interface Menuitem{
+  texto:string,
+  ruta:string
+}
 @Component({
   selector: 'app-sidemenu',
   templateUrl: './sidemenu.component.html',
@@ -7,6 +11,35 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class SidemenuComponent implements OnInit {
+   
+  templateMenu: Menuitem[]=[
+    {
+      texto:'Básicos',
+      ruta:'./template/basicos'
+    },
+    {
+      texto:'Dinámicos',
+      ruta:'./template/dinamicos'
+    },
+    {
+      texto:'Switches',
+      ruta:'./template/switches'
+    }
+  ]
+  reactiveMenu: Menuitem[]=[
+    {
+      texto:'Básicos',
+      ruta:'./reactive/basicos'
+    },
+    {
+      texto:'Dinámicos',
+      ruta:'./reactive/dinamicos'
+    },
+    {
+      texto:'Switches',
+      ruta:'./reactive/switches'
+    }
+  ]
 
   constructor() { }
 
