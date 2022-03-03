abbaa09bdf4aa45c36d1393a8f608c636300c478
alejahndro02 <alejahndro02@gmail.com>
2022-03-02 20:37:06 -0600

    Se crearon unas interfaces y se declaro una propiedad persona de tipo persona asignandoles los valores

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+// Se crea la interface para la data que se utilizara 
+interface Persona{
+  nombre: string,
+  favoritos: Favoritos[]
+}
+
+interface Favoritos{
+  id:number
+  nombreJuego:string
+}
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
@@ -7,7 +18,14 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class DinamicosComponent implements OnInit {
+persona: Persona = {
+  nombre:'Quetzalli',
+  favoritos: [
+    {id:1, nombreJuego:'Masha'},
+    {id:2, nombreJuego:'Marinerito'}
 
+  ]
+}
 
   constructor() { }
 
