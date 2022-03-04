33e987b8d33d566a33437f67e0f7fdaeb82b9bd9
alejahndro02 <alejahndro02@gmail.com>
2022-03-04 17:55:22 -0600

    Se declara el metodo de guardarRegistro

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -39,6 +39,14 @@ export class BasicosComponent implements OnInit {
   constructor( private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    /* Con el metodo reset se establecen valores prederminados y se delcaran dentro del ngOnInit para que se inicialice 
+    con esos valores al cargar el componente*/
+    /*Otro metodo es usar setvalue pero tiene el inconveniente que si le falta un campo la app truena y con el metodo reset no 
+    por eso para este ejemplo se usa reset*/ 
+    this.miFomularioReactivo.reset({
+      nombreProductoReactive:'Audifonos',
+      precioReactivo:300,
+    })
   }
   /*Se le pasa como argumento al metodo un elemento de tipo sring en este caso es un 
    campo que en el htmml se le pasa el campo que se desea evaluar*/
@@ -46,5 +54,16 @@ export class BasicosComponent implements OnInit {
     return this.miFomularioReactivo.controls[campo].errors && 
            this.miFomularioReactivo.controls[campo].touched
   }
-
+  guardarRegistro(){
+    if(this.miFomularioReactivo.invalid){
+      /* con el metodo markAllTouched se evalua si los campos no han sido tocados mandara el 
+      mensaje de error en cada uno de los elementos del formulario*/
+      this.miFomularioReactivo.markAllAsTouched();
+      return
+    }
+    console.log('algo', this.miFomularioReactivo.value);
+    // Se restablecen los valores del formulario
+    this.miFomularioReactivo.reset()
+    
+  }
 }
