a2a440ad6ac22ad3d9903ee9763d77b4a7c367ec
alejahndro02 <alejahndro02@gmail.com>
2022-03-05 10:37:42 -0600

    Se agrego una nueva propiedad agregarFavs, se declaro el metodo agregarRegistro y eliminarReistro

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,6 +1,7 @@
 import { Component, 
          OnInit     } from '@angular/core';
 import { FormArray, FormBuilder, 
+         FormControl, 
          FormGroup, 
          Validators } from '@angular/forms';
 
@@ -14,25 +15,59 @@ export class DinamicosComponent implements OnInit {
 
   formularioDinamicoReactivo: FormGroup = this.fb.group({
     nombre:[, [Validators.required,
-                Validators.minLength(3)]],
+                Validators.minLength(3)]
+    ],
     favoritos: this.fb.array( [
-      ['Masha', Validators.required],
-      ['metalGear', Validators.required]
-    ], Validators.required)
+        ['Masha', Validators.required],
+        ['metalGear', Validators.required]
+      ], Validators.required
+    )
   })
-get favoritosArr(){
-  return this.formularioDinamicoReactivo.get('favoritos') as FormArray;
-}
+  //Se delcara una nueva propiedad incependiente al formulario para recibir la informacion que se desea agregar
+  agregarFavs: FormControl= this.fb.control('', Validators.required)
+
+  get favoritosArr(){
+    return this.formularioDinamicoReactivo.get('favoritos') as FormArray;
+  }
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    console.log(this.favoritosArr);
+    console.log(this.favoritosArr.controls[0].value);
     
   }
+
+  agregarRegistro(){
+    if(this.agregarFavs.invalid){
+      return
+    }
+    //este es lo mismo si se hace con el formBuilder /*this.favoritosArr.push(new FormControl)*/
+    /*this.favoritosArr.push(
+      new FormControl(
+        this.agregarFavs.value,
+        Validators.required
+      )
+    )*/
+    /*Al metodo push se agrega el control por medio de formBuilder y se le pasas como argumento el vaor de lo que proviene de 
+     la propiedad agergarFavs*/
+    this.favoritosArr.push(
+      this.fb.control(
+        this.agregarFavs.value,
+        Validators.required
+      )
+    )
+
+    this.agregarFavs.reset()
+  }
+
   campoNoValido(campo:string){
     return this.formularioDinamicoReactivo.controls[campo].errors && 
            this.formularioDinamicoReactivo.controls[campo].touched
   }
+
+  eliminarRegistro(i: number){
+    this.favoritosArr.removeAt(i)
+  }
+
   guardar(){
     if(this.formularioDinamicoReactivo.invalid){
       /* con el metodo markAllTouched se evalua si los campos no han sido tocados mandara el 
@@ -43,5 +78,6 @@ get favoritosArr(){
     console.log('algo', this.formularioDinamicoReactivo.value);
     // Se restablecen los valores del formulario
     this.formularioDinamicoReactivo.reset()
+    this.agregarFavs.reset()
   }
 }
