09cfcc7977ec161bd38bf3a4c718246d2712ca82
alejahndro02 <alejahndro02@gmail.com>
2022-03-06 09:20:04 -0600

    Se decalro la propiedad formularioSwitchDinamico y se agregaron sus valores mediante formBuilder, se creo un objeto tipo persona donde solo tendra el genero y notificaciones, se decalro el metodo guardar() que elimina el campo de teminos de la propiedad  formValue y el resultado se asigna al mismo objeto formValue, en el metodo ngOnInit se evalua si ahy algun cambio dentro del formulario si es true ejecuta el metodo subscribe

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, 
+         OnInit     } from '@angular/core';
+import { FormBuilder, 
+         FormGroup, 
+         Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -6,11 +10,43 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
+
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  formularioSwitchDinamico: FormGroup = this.fb.group({
+    genero        :[ 'M'   , Validators.required ],
+    notificaciones:[  true  , Validators.required ],
+    terminos      :[  false , Validators.requiredTrue ]
+  })
+
+  persona={
+    genero:'F',
+    notificaciones:true
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+
+    this.formularioSwitchDinamico.reset({
+      ...this.persona,
+      terminos:false
+    })
+    /*Otro ejemplo de poder hacerlo es 
+    this.formularioSwitchDinamico.get('terminos')?.valueChanges.subscribe(nuevoValor => {
+      console.log(nuevoValor)
+    }) */
+// Se susbcribe a los cambios dentro del fomrmulario, se extrae el elemento de termino 
+    this.formularioSwitchDinamico.valueChanges.subscribe( ({ terminos, ...restoArgumentos }) => {
+      this.persona= restoArgumentos
+    })
+
+  }
+
+  guardar(){
+    const formValue={ ...this.formularioSwitchDinamico.value }
+    delete formValue.terminos
+    this.persona = formValue
   }
 
 }
