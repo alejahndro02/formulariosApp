e005dc69efc9394a453dd493ad9b8885285b92d2
alejahndro02 <alejahndro02@gmail.com>
2022-03-06 13:48:22 -0600

    Se decalro una propiedad nombre Pattern que tiene un valor con las reglas que tendra el elemento, se creo la propiedad miformulario y se inyecto el servicio de formBuilder y por ultimo dse decalraron dos metodos camp no valido que evalua si el campo es invalido y si ha sido tocado y submitForm que evalua si el formulario es invalido y si tiene como true al ejecutar markAllAsTouched

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, 
+         OnInit       } from '@angular/core';
+import { FormBuilder, 
+         FormGroup, 
+         Validators   } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -7,10 +11,33 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class RegistroComponent implements OnInit {
+  // exprecion temporal se evalua que el nombre tenga letras de la a la z al igual que el segundo campo que corresponde al apellido
+  nombrePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
 
-  constructor() { }
+  miFormulario:FormGroup= this.fb.group({
+    nombre:['',[ Validators.required, 
+                 Validators.pattern( this.nombrePattern )
+                ]
+            ]
+    // nombre:['',Validators.required ]
+
+  })
+
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
   }
+  campoNoValido( campo:string ){
 
+    return this.miFormulario.get(campo)?.invalid
+    && this.miFormulario.get(campo)?.touched
+  }
+  submitForm(){
+    if( this. miFormulario.invalid){
+      this.miFormulario.markAllAsTouched()
+      return
+    }
+    console.log(this.miFormulario.value);
+    
+  }
 }
