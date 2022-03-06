d32e5a0aa78c348bf9637b1f0c4d4ea56f55a730
alejahndro02 <alejahndro02@gmail.com>
2022-03-06 13:43:50 -0600

    Se importo el reactiveFormModule

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,9 +1,10 @@
-import { NgModule           } from '@angular/core';
-import { CommonModule       } from '@angular/common';
+import { NgModule             } from '@angular/core';
+import { ReactiveFormsModule  } from '@angular/forms';
+import { CommonModule         } from '@angular/common';
 
-import { AuthRoutingModule  } from './auth-routing.module';
-import { LoginComponent     } from './pages/login/login.component';
-import { RegistroComponent  } from './pages/registro/registro.component';
+import { AuthRoutingModule    } from './auth-routing.module';
+import { LoginComponent       } from './pages/login/login.component';
+import { RegistroComponent    } from './pages/registro/registro.component';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { RegistroComponent  } from './pages/registro/registro.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
