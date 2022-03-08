4e160527be1b1b7e64198b3aff2e24f02805316e
alejahndro02 <alejahndro02@gmail.com>
2022-03-07 22:48:34 -0600

    Se importa el modulo de httpClientModule dentro de la raiz del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule         } from '@angular/core';
+import { BrowserModule    } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 import { SharedModule     } from '@shared/shared.module';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -13,7 +13,8 @@ import { AppComponent     } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    HttpClientModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
