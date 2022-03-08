8eac7452dbe9a27b7a0a5b1d84eb48c14f852360
alejahndro02 <alejahndro02@gmail.com>
2022-03-07 22:22:41 -0600

    Se declaro la propiedad y Validaors donde se ejecuta el metodo camposIguales y se pasas como parametros la contaseña y la configuracion

--- src/app/shared/validators/validator.service.ts
+++ src/app/shared/validators/validator.service.ts
@@ -1,12 +1,16 @@
-import { Injectable } from '@angular/core';
-import { FormControl, ValidationErrors } from '@angular/forms';
+import { Injectable       } from '@angular/core';
+import { AbstractControl, 
+         FormControl, 
+         ValidationErrors } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ValidatorService {
+  
   nombrePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
   emailPattern : string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+  
   constructor() { }
   
   noPuedeSerStrider(control: FormControl): ValidationErrors | null{
@@ -18,5 +22,30 @@ export class ValidatorService {
       }
     }
     return null;
-}
+  }
+  camposIguales( pass:string, conf:string ){
+
+    return (formGroup:AbstractControl):ValidationErrors | null => {
+      
+      const pwd= formGroup.get(pass)?.value
+      const cnf= formGroup.get(conf)?.value  
+      console.log(pwd, cnf);
+      
+      if( pwd !== cnf){
+        // se establece el error donde no coinciden los campos 
+        formGroup.get('confirmar')?.setErrors({
+          noiguales:true
+        })
+        //si se retorna un objeto es que hubo un error
+        // return {
+        //   noIguales:true
+        // }
+      }
+      
+      formGroup.get('confirmar')?.setErrors(null)
+
+      // Si se retorna null es que no hubo ningun error 
+      return null
+    }
+  }
 }
