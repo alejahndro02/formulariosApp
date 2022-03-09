4a5812ed2296f1a360f48de9ba9700d9c2e90e7e
alejahndro02 <alejahndro02@gmail.com>
2022-03-09 00:02:04 -0600

    Se importo y se implemento el AsyncValidatory se declara el metodo validate que tiene como parametro el control aque es el valor del formulario

--- src/app/shared/validators/email-validator.service.ts
+++ src/app/shared/validators/email-validator.service.ts
@@ -1,9 +1,35 @@
-import { Injectable } from '@angular/core';
+import { HttpClient         } from '@angular/common/http';
+import { Injectable         } from '@angular/core';
+import { AbstractControl, 
+         AsyncValidator, 
+         ValidationErrors   } from '@angular/forms';
+import { map, 
+         Observable         } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class EmailValidatorService {
 
-  constructor() { }
+// Se implementa el AsyncValidator para hacer una peticion asincrona que dependa de un servicio adicional como http
+export class EmailValidatorService implements AsyncValidator {
+
+  constructor(  private http: HttpClient  ) { }
+  
+  /* Este metodo se ejecuta al imlementar el AsyncValidator se puede manejar como promesa o como observable ene este caso 
+  se utilizara el observble*/
+  validate( control: AbstractControl ): Observable<ValidationErrors | null> {
+    
+    const email =  control.value
+    
+    /*Con el operador map se transforma el valor de la informacion que emite el observable de la peticion http */
+    return this.http.get<any[]>( `http://localhost:3000/usuarios?q=${ email }` )
+          .pipe(
+            map( response => {
+              return (  response.length === 0)
+                ? null
+                :{ emailMatch:true}
+            })
+          )
+            
+  }
 }
