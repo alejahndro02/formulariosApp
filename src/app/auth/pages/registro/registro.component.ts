55eaa4807c802f9c50f90bcaf2d543cc1b4325b8
alejahndro02 <alejahndro02@gmail.com>
2022-03-09 00:05:18 -0600

    Se inyecto el servicio de emailService atraves del contructor y es llamado como tercer argumento dentro de la propiedad email que pertenece a los validaciones asincronas y esta se manda a llaar con this.emailValidator

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,11 +1,13 @@
 import { Component, 
-         OnInit             } from '@angular/core';
+         OnInit                } from '@angular/core';
 
 import { FormBuilder, 
          FormGroup, 
-         Validators         } from '@angular/forms';
-        
-import { ValidatorService   } from '@shared/validators/validator.service';
+         Validators            } from '@angular/forms';
+         
+import { EmailValidatorService } from '@shared/validators/email-validator.service';
+import { ValidatorService      } from '@shared/validators/validator.service';
+
 //Se usa en caso de solo usar el archivo de validaciones
 // import { emailPattern, 
 //          nombrePattern,  
@@ -22,15 +24,20 @@ export class RegistroComponent implements OnInit {
 
   // si solo se usa el validaciones se aueda asi Validators.pattern( nombrePattern ) los campos de las propiedades
   // si se agrega el sevicio se añade el this.validatorService a la propieda del objeto
+  
   miFormulario:FormGroup= this.fb.group({
-    nombre:['',[ Validators.required, 
-                 Validators.pattern( this.validatorService.nombrePattern )
-              ]
-            ],
-    email:['',[ Validators.required,
-                Validators.pattern( this.validatorService.emailPattern )
-              ]
-          ],
+    /* Sintaxis propiedad:['valor',[validacionesSincronas], [validacionesAsincronas]] que no dependen 
+    de peticiones http, siempre y cuando regresen una promesa u observable*/
+  
+    nombre        :['',[ Validators.required, 
+                         Validators.pattern( this.validatorService.nombrePattern )
+                      ]
+                    ],
+    email         :['',[ Validators.required,
+                         Validators.pattern( this.validatorService.emailPattern )
+                       ], 
+                       [  this.emailValidator ]
+                   ],
           // El straider es la palabra que no quiero que coicida el campo 
     nombreUsuario :['', [ Validators.required,
                         this.validatorService.noPuedeSerStrider]
@@ -42,17 +49,19 @@ export class RegistroComponent implements OnInit {
                     ],
   },{
     // Se definen las validaciones que aplicaran al formulario miFormulario
-    validators:[this.validatorService.camposIguales('contraseña', 'confirmar')]
+    validators    :[this.validatorService.camposIguales('contraseña', 'confirmar')]
   })
 
-  constructor(private fb:FormBuilder, private validatorService:ValidatorService) { }
+  constructor( private fb:FormBuilder, 
+               private validatorService:ValidatorService,
+               private emailValidator: EmailValidatorService ) { }
 
   ngOnInit(): void {
     // VAlores por defecto
     this.miFormulario.reset({
-      nombre:'Quetzalli Hernandez',
-      email:'quetzitachi@gmail.com',
-      nombreUsuario:'Quetzitachi'
+      nombre        :'Quetzalli Hernandez',
+      email         :'quetzitachi@gmail.com',
+      nombreUsuario :'Quetzitachi'
     })
     // console.log(this.miFormulario.value);
     
@@ -60,7 +69,7 @@ export class RegistroComponent implements OnInit {
   campoNoValido( campo:string ){
 
     return this.miFormulario.get(campo)?.invalid
-    && this.miFormulario.get(campo)?.touched
+        && this.miFormulario.get(campo)?.touched
   }
   submitForm(){
     if( this. miFormulario.invalid){
